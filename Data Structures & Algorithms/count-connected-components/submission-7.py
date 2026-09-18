f069d5bc8b5cdb0900fc6ecbf8285c1dae24fedb
Jerry Wong <187459546+spestivekirb@users.noreply.github.com>
2026-09-18 10:46:29 -0400

    Add: count-connected-components - submission-7

---
+++ Data Structures & Algorithms/count-connected-components/submission-7.py
@@ -0,0 +1,32 @@
+class DSU:
+    def __init__(self, n):
+        self.par = [i for i in range(n)]
+        self.rank = [1] * n
+
+    def find(self, i):
+        if self.par[i] == i:
+            return i
+        self.par[i] = self.find(self.par[i])
+        return self.par[i]
+
+    def union(self, a, b):
+        pa = self.find(a)
+        pb = self.find(b)
+        if pa == pb:
+            return False
+
+        if self.rank[pa] > self.rank[pb]:
+            pa, pb = pb, pa
+        self.par[pb] = pa
+        self.rank[pa] += self.rank[pb]
+        return True
+
+class Solution:
+    def countComponents(self, n: int, edges: List[List[int]]) -> int:
+        dsu = DSU(n)
+        res = n
+        for u, v in edges:
+            if dsu.union(u, v):
+                res -= 1
+        return res
+
